aa5cd9a0ba065c1f17fe0e9a5f7931645232d91b
j.berlin <j.berlin@ewerk.com>
2025-12-20 17:31:04 +0100

    Add ext_localconf.php to override ProfileSearch properly

---
+++ Classes/Search/BistumsligaProfileSearch.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Derhecht\Bistumsliga\Search;
+
+use System25\T3sports\Search\ProfileSearch;
+
+class BistumsligaProfileSearch extends ProfileSearch
+{
+    protected function getTableMappings()
+    {
+        $tableMapping = [
+            'PROFILE' => 'tx_cfcleague_profiles',
+            'TEAM' => 'tx_cfcleague_teams',
+            'TEAMNOTE' => 'tx_cfcleague_team_notes',
+        ];
+        // Hook to append other tables
+        Misc::callHook('cfc_league', 'search_Profile_getTableMapping_hook', [
+            'tableMapping' => &$tableMapping,
+        ], $this);
+
+        return $tableMapping;
+    }
+
+    protected function getJoins($tableAliases)
+    {
+        $join = [];
+        if (isset($tableAliases['TEAM'])) {
+            $join[] = new Join('PROFILE', 'tx_cfcleague_teams', 'FIND_IN_SET(PROFILE.uid, TEAM.players)', 'TEAM');
+        }
+        if (isset($tableAliases['TEAMNOTE'])) {
+            $join[] = new Join('PROFILE', 'tx_cfcleague_team_notes', 'PROFILE.uid = TEAMNOTE.player', 'TEAMNOTE');
+        }
+
+        // Hook to append other tables
+        Misc::callHook('cfc_league', 'search_Profile_getJoins_hook', [
+            'join' => &$join,
+            'tableAliases' => $tableAliases,
+        ], $this);
+
+        return $join;
+    }
+}
---
+++ ext_localconf.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][System25\T3sports\Search\ProfileSearch::class] = [
+        'className' => \Derhecht\Bistumsliga\Search\BistumsligaProfileSearch::class
+];
