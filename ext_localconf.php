3ec1e1b9695c102427ed88dde3bc2995336afd31
j.berlin <j.berlin@ewerk.com>
2026-01-03 18:12:37 +0100

    Remove BistumsligaProfileSearch class and related Extbase class mapping

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,9 +1,5 @@
 <?php
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][System25\T3sports\Search\ProfileSearch::class] = [
-        'className' => \Derhecht\Bistumsliga\Search\BistumsligaProfileSearch::class
-];
-
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][System25\T3sports\Frontend\Marker\MatchMarker::class] = [
     'className' => \Derhecht\Bistumsliga\Frontend\Marker\BistumsligaMatchMarker::class
 ];
